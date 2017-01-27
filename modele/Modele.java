6cfa7f029c2e2c2e48e90930930fb736c81a5103
Thibault <th.delavelle@gmail.com>
2017-01-27 18:08:55 +0100

    cette fois je ne met à jour que les voisins 4-connexes, et j'ai modifié la fonction gausienne, je ne sais pas si le resultat est bon ou pas

--- modele/Modele.java
+++ modele/Modele.java
@@ -12,8 +12,8 @@ public class Modele {
 	
 	public Neurone[][] carte;
 	public ArrayList<Neurone> entrees;
-	public double mu = 5.;
-	public double sigma = 2.;
+	public double mu = 5.; //pas d'apprentissage decroissant
+	public double sigma = 2.; //ecart type deccroissant
 	
 	public Modele(int width, int length){
 		this.carte = new Neurone[width][length];
@@ -50,24 +50,69 @@ public class Modele {
 		for (int i = 0; i < entrees.size() ;i++) {
 			Neurone xi = entrees.get(i);
 			double min = Integer.MAX_VALUE;
+			int xMin = 0;
+			int yMin = 0;
 			//Récuperation du neurone de la carte le plus proche
 			for (int x = 0; x < carte.length; x++) {
 				for (int y = 0; y < carte.length; y++) {
 					Neurone ni = carte[x][y];
 					double distance = getDistance(xi, ni);
-					if(distance < min)min = distance;
+					if(distance < min){
+						min = distance;
+						xMin = x;
+						yMin = y;
+					}
 				}
 			}
 			
-			//Mise à jour des poids pour chaque neurone de la carte
-			for (int x = 0; x < carte.length; x++) {
-				for (int y = 0; y < carte.length; y++) {
-					carte[x][y].x1 += mu * gausienne(min);
-					carte[x][y].x2 += mu * gausienne(min);
-					carte[x][y].x3 += mu * gausienne(min);
-					carte[x][y].x4 += mu * gausienne(min);
-				}
+			// mise à jour de ivainqueur
+			carte[xMin][yMin].x1 += mu * gausienne((carte[xMin][yMin]),xi);
+			carte[xMin][yMin].x2 += mu * gausienne((carte[xMin][yMin]),xi);
+			carte[xMin][yMin].x3 += mu * gausienne((carte[xMin][yMin]),xi);
+			carte[xMin][yMin].x4 += mu * gausienne((carte[xMin][yMin]),xi);
+			
+			
+			//mise à jour de ses voisins proches (ses voisins en 4-connexité) 
+			if(xMin<carte.length-1){
+				carte[xMin+1][yMin].x1 += mu * gausienne((carte[xMin+1][yMin]),xi);
+				carte[xMin+1][yMin].x2 += mu * gausienne((carte[xMin+1][yMin]),xi);
+				carte[xMin+1][yMin].x3 += mu * gausienne((carte[xMin+1][yMin]),xi);
+				carte[xMin+1][yMin].x4 += mu * gausienne((carte[xMin+1][yMin]),xi);
 			}
+			
+			if(xMin > 0){
+				carte[xMin-1][yMin].x1 += mu * gausienne((carte[xMin-1][yMin]),xi);
+				carte[xMin-1][yMin].x2 += mu * gausienne((carte[xMin-1][yMin]),xi);
+				carte[xMin-1][yMin].x3 += mu * gausienne((carte[xMin-1][yMin]),xi);
+				carte[xMin-1][yMin].x4 += mu * gausienne((carte[xMin-1][yMin]),xi);
+			}
+			
+			if(yMin<carte.length-1){
+				carte[xMin][yMin+1].x1 += mu * gausienne((carte[xMin][yMin+1]),xi);
+				carte[xMin][yMin+1].x2 += mu * gausienne((carte[xMin][yMin+1]),xi);
+				carte[xMin][yMin+1].x3 += mu * gausienne((carte[xMin][yMin+1]),xi);
+				carte[xMin][yMin+1].x4 += mu * gausienne((carte[xMin][yMin+1]),xi);
+			}
+			
+			if(yMin > 0){
+				carte[xMin][yMin-1].x1 += mu * gausienne((carte[xMin][yMin-1]),xi);
+				carte[xMin][yMin-1].x2 += mu * gausienne((carte[xMin][yMin-1]),xi);
+				carte[xMin][yMin-1].x3 += mu * gausienne((carte[xMin][yMin-1]),xi);
+				carte[xMin][yMin-1].x4 += mu * gausienne((carte[xMin][yMin-1]),xi);
+			}
+			
+			
+			
+			//Mise à jour des poids pour chaque neurone de la carte
+			/*for (int x = -1; x <= 1; x++) {
+				for (int y = -1; y <= 1 ; y++) {
+					carte[x][y].x1 += mu * gausienne((double)carte[x][y].x1-xi.x1);
+					carte[x][y].x2 += mu * gausienne((double)carte[x][y].x2-xi.x2);
+					carte[x][y].x3 += mu * gausienne((double)carte[x][y].x3-xi.x3);
+					carte[x][y].x4 += mu * gausienne((double)carte[x][y].x4-xi.x4);
+				}
+			}*/
+			
 			afficherCarte();
 			mu -= 0.01;
 			sigma -= 0.01;
@@ -77,8 +122,15 @@ public class Modele {
 	}
 	
 	
-	public double gausienne(double x){
-		return Math.exp(-((x*x)/(Math.pow(2*sigma,2))));
+	public double gausienne(Neurone x, Neurone y){
+		
+		double distance = getDistance(x, y);
+		distance = distance*distance;
+		
+		double ecart = (2*sigma)*(2*sigma);
+		
+		return Math.exp(-(distance/ecart));
+		//return Math.exp(-((x*x)/(Math.pow(2*sigma,2))));
 		
 	}
 	
@@ -87,11 +139,11 @@ public class Modele {
 		
 			BufferedReader br2;
 			String st;
-
+			int cmpt=0;
 			//System.out.println("Lecture fichier... ");
 			String[] separated;
 			br2 = new BufferedReader(new FileReader(fichier));
-			while ((st = br2.readLine()) != null) {
+			while ((st = br2.readLine()) != null && cmpt < 10) {
 				separated = st.split(",");
 				double x1 = Double.parseDouble(separated[0]);
 				double x2 = Double.parseDouble(separated[1]);
@@ -99,6 +151,7 @@ public class Modele {
 				double x4 = Double.parseDouble(separated[3]);
 				Neurone n = new Neurone(x1,x2,x3,x4);
 				entrees.add(n);
+				cmpt++;
 			}
 			br2.close();
 	}
@@ -111,11 +164,10 @@ public class Modele {
 				Random r = new Random();
 				Neurone n = new Neurone();
 				carte[i][j] = n;
-				carte[i][j].x1= r.nextInt(10)-10;
-				carte[i][j].x2= r.nextInt(10)-10;
-				carte[i][j].x3= r.nextInt(10)-10;
-				carte[i][j].x4= r.nextInt(10)-10;
-				
+				carte[i][j].x1= r.nextInt(10);
+				carte[i][j].x2= r.nextInt(10);
+				carte[i][j].x3= r.nextInt(10);
+				carte[i][j].x4= r.nextInt(10);
 			}
 		}
 	}
@@ -133,7 +185,7 @@ public class Modele {
 	//affichage carte
 	public String toString(){
 		StringBuilder st = new StringBuilder();
-		st.append("Carte de Kohonen : \n");
+		st.append("Carte de Kohonen (pour chaque neurone I on a (i1,i2,i3,i4)) 8x8: \n");
 		for (int i = 0; i < carte.length; i++) {
 			for (int j = 0; j < carte[0].length; j++) {
 				st.append(carte[i][j].toString());
